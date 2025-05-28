65a240ce93ec3a2cba4e7aca653c11cc7556045f
Rohan <rohan.2021b0131077@abes.ac.in>
2025-05-28 16:38:41 +0530

    add lessons

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import React, { useState } from 'react'
-import './App.css'
-import Academy from './Academy'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CourseGrid from './components/CourseGrid';
-import AcademyHelpPage from './components/AcademyHelpPage';
+import React, { useState } from "react";
+import "./App.css";
+import Academy from "./Academy";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CourseGrid from "./components/CourseGrid";
+import AcademyHelpPage from "./components/AcademyHelpPage";
+import LessonsPage from "./components/Lessons/LessonsPage";
+import LessonContent from "./components/Lessons/LessonContent";
+
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Academy />} />
         <Route path="/courses" element={<CourseGrid />} />
+        <Route path="/courses/:courseId" element={<LessonsPage />} />
         <Route path="/help" element={<AcademyHelpPage />} />
-
+        <Route
+          path="/courses/:courseId/lessons/:lessonId"
+          element={
+            <LessonContent
+              youtubeUrl="https://www.youtube.com/watch?v=9B4CvtzXRpc"
+              pdfUrl="https://www.newline.co/fullstack-react/assets/media/sGEMe/MNzue/30-days-of-react-ebook-fullstackio.pdf"
+              courseTitle="ReactJS Beginner to Advanced"
+            />
+          }
+        />
       </Routes>
       {/* <Academy/> */}
     </>
   );
 }
 
-export default App
+export default App;
